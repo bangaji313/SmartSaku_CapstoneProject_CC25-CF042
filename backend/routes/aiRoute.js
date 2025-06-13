3dd25bdab64f78407c154f3ef308dcad6b4933cd
Mabruri101 <muhammadmabruri99@gmail.com>
2025-06-12 23:17:47 -0400

    tambahkan rekomendasi dan prediksi

--- backend/routes/aiRoute.js
+++ backend/routes/aiRoute.js
@@ -1,10 +1,20 @@
-import { chatHandler } from "../handler/aiHandler.js";
+import { chatHandler, predictionHandler, recommendationHandler } from "../handler/aiHandler.js";
 
 const aiRoutes = [
   {
     method: "POST",
     path: "/chat",
     handler: chatHandler,
+  },
+  {
+    method: "POST",
+    path: "/recommendation",
+    handler: recommendationHandler,
+  },
+  {
+    method: "GET",
+    path: "/prediction",
+    handler: predictionHandler,
   },
 ];
 
